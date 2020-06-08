ffc425a12bbe80bffdf5a7363804a5feea86c804
Carina Codorean <carina.codorean@yahoo.com>
2020-06-08 13:59:47 +0300

    Carina

--- src/components/EditProfileTable/EditProfileTable.js
+++ src/components/EditProfileTable/EditProfileTable.js
@@ -12,85 +12,85 @@ import Button from "@material-ui/core/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const useStyles = makeStyles({
-    table: {
-        minWidth: 650,
-    },
+  table: {
+    minWidth: 650,
+  },
 });
 
 export default function SimpleTable() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const [id, setId] = useState("");
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [photo, setPhoto] = useState("");
-    const [desc, setDesc] = useState("");
-    const [phone, setPhone] = useState("");
-    const [city, setCity] = useState("");
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [photo, setPhoto] = useState("");
+  const [desc, setDesc] = useState("");
+  const [phone, setPhone] = useState("");
+  const [city, setCity] = useState("");
 
-    useEffect(() => {
-        const requestOptions = {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                token: localStorage.getItem("jwt-token"),
-            },
-        };
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        token: localStorage.getItem("jwt-token"),
+      },
+    };
 
-        fetch("http://localhost:4000/user/me", requestOptions)
-            .then((response) => response.json())
-            .then((data) => handleResponse(data));
-    }, []);
+    fetch("http://localhost:4000/user/me", requestOptions)
+      .then((response) => response.json())
+      .then((data) => handleResponse(data));
+  }, []);
 
-    function handleResponse(data) {
-        setId(data._id);
-        setName(data.username);
-        setEmail(data.email);
-        //setPhoto(data.photo);
-        setCity(data.city);
-        setDesc(data.desc);
-        setPhone(data.phone);
-    }
+  function handleResponse(data) {
+    setId(data._id);
+    setName(data.username);
+    setEmail(data.email);
+    //setPhoto(data.photo);
+    setCity(data.city);
+    setDesc(data.desc);
+    setPhone(data.phone);
+  }
 
-    function handleSubmit(event) {
-        setName({ name: event.target.value });
-        setEmail({ email: event.target.value });
-        setPhoto({ photo: event.target.value });
-        setCity({ city: event.target.value });
-        setDesc({ desc: event.target.value });
-        setPhone({ phone: event.target.value });
-    }
+  function handleSubmit(event) {
+    setName({ name: event.target.value });
+    setEmail({ email: event.target.value });
+    setPhoto({ photo: event.target.value });
+    setCity({ city: event.target.value });
+    setDesc({ desc: event.target.value });
+    setPhone({ phone: event.target.value });
+  }
 
-    return (
-        <Form>
-            <Form.Group controlId="exampleForm.ControlInput1">
-                <Form.Label>Full Name</Form.Label>
-                <Form.Control type="text" placeholder={name} value={name} />
-            </Form.Group>
-            <Form.Group controlId="exampleForm.ControlInput1">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder={email} value={email} />
-            </Form.Group>
-            <Form.Group>
-                <Form.File
-                    id="exampleFormControlFile1"
-                    label="Profile photo"
-                    value={photo}
-                />
-            </Form.Group>
-            <Form.Group controlId="exampleForm.ControlInput1">
-                <Form.Label>Phone number</Form.Label>
-                <Form.Control type="text" placeholder={phone} value={phone} />
-            </Form.Group>
-            <Form.Group controlId="exampleForm.ControlInput1">
-                <Form.Label>City</Form.Label>
-                <Form.Control type="text" placeholder={city} value={city} />
-            </Form.Group>
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Description</Form.Label>
-                <Form.Control as="textarea" rows="3" placeholder={desc} value={desc} />
-            </Form.Group>
-            <Button onClick={handleSubmit}>Update profile</Button>
-        </Form>
-    );
+  return (
+    <Form>
+      <Form.Group controlId="exampleForm.ControlInput1">
+        <Form.Label>Full Name</Form.Label>
+        <Form.Control type="text" placeholder={name} value={name} />
+      </Form.Group>
+      <Form.Group controlId="exampleForm.ControlInput1">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder={email} value={email} />
+      </Form.Group>
+      <Form.Group>
+        <Form.File
+          id="exampleFormControlFile1"
+          label="Profile photo"
+          value={photo}
+        />
+      </Form.Group>
+      <Form.Group controlId="exampleForm.ControlInput1">
+        <Form.Label>Phone number</Form.Label>
+        <Form.Control type="text" placeholder={phone} value={phone} />
+      </Form.Group>
+      <Form.Group controlId="exampleForm.ControlInput1">
+        <Form.Label>City</Form.Label>
+        <Form.Control type="text" placeholder={city} value={city} />
+      </Form.Group>
+      <Form.Group controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Description</Form.Label>
+        <Form.Control as="textarea" rows="3" placeholder={desc} value={desc} />
+      </Form.Group>
+      <Button onClick={handleSubmit}>Update profile</Button>
+    </Form>
+  );
 }
--- src/components/Header/HeaderLinks.js
+++ src/components/Header/HeaderLinks.js
@@ -3,16 +3,17 @@ import React from "react";
 import DeleteIcon from "@material-ui/icons/Delete";
 import IconButton from "@material-ui/core/IconButton";
 // react components for routing our app without refresh
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 // @material-ui/core components
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Tooltip from "@material-ui/core/Tooltip";
 
 // @material-ui/icons
-import {Apps, CloudDownload} from "@material-ui/icons";
+import { Apps, CloudDownload } from "@material-ui/icons";
 
 // core components
 import CustomDropdown from "components/CustomDropdown/CustomDropdown.js";
@@ -24,73 +25,96 @@ import auth from "../../auth";
 const useStyles = makeStyles(styles);
 
 export default function HeaderLinks(props) {
-    const classes = useStyles();
+  const classes = useStyles();
+  const [name, setName] = useState("");
 
-    function logOut() {
-        auth.logout();
-    }
+  function logOut() {
+    auth.logout();
+  }
 
-    return (
-        <List className={classes.list}>
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        token: localStorage.getItem("jwt-token"),
+      },
+    };
 
-            <ListItem className={classes.listItem}>
-                <Button
-                    href="/"
-                    color="transparent"
-                    className={classes.navLink}
-                >
-                    Home
-                </Button>
-            </ListItem>
+    fetch("http://localhost:4000/user/me", requestOptions)
+      .then((response) => response.json())
+      .then((data) => handleResponse(data));
+  }, []);
+  function handleResponse(data) {
+    setName(data.username);
+  }
+  return (
+    <List className={classes.list}>
+      <ListItem className={classes.listItem}>
+        <Button href="/" color="transparent" className={classes.navLink}>
+          Home
+        </Button>
+      </ListItem>
+      {auth.isAuthenticated() && (
+        <ListItem className={classes.listItem}>
+          <Button
+            href="/login-page"
+            color="transparent"
+            className={classes.navLink}
+          >
+            Welcome, {name}!
+          </Button>
+        </ListItem>
+      )}
+      {!auth.isAuthenticated() && (
+        <ListItem className={classes.listItem}>
+          <Button
+            href="/login-page"
+            color="transparent"
+            className={classes.navLink}
+          >
+            Login
+          </Button>
+        </ListItem>
+      )}
+      {auth.isAuthenticated() && (
+        <ListItem className={classes.listItem}>
+          <Button
+            href="/login-page"
+            color="transparent"
+            className={classes.navLink}
+            onClick={logOut}
+          >
+            Logout
+          </Button>
+        </ListItem>
+      )}
 
-            {!auth.isAuthenticated() &&
-            <ListItem className={classes.listItem}>
-                <Button
-                    href="/login-page"
-                    color="transparent"
-                    className={classes.navLink}
-                >
-                    Login
-                </Button>
-            </ListItem>
-            }
-            {auth.isAuthenticated() &&
-            <ListItem className={classes.listItem}>
-                <Button
-                    href="/login-page"
-                    color="transparent"
-                    className={classes.navLink}
-                    onClick={logOut}
-                >
-                    Logout
-                </Button>
-            </ListItem>
-            }
-            <ListItem className={classes.listItem}>
-                <CustomDropdown
-                    noLiPadding
-                    buttonText="More"
-                    buttonProps={{
-                        className: classes.navLink,
-                        color: "transparent"
-                    }}
-                    buttonIcon={Apps}
-                    dropdownList={[
-                        <Link to="/profile-page" className={classes.dropdownLink}>
-                            Profile
-                        </Link>,
-                        <Link to="/adopt-page" className={classes.dropdownLink}>
-                            Adopt
-                        </Link>,
-                        <Link to="/lost-page" className={classes.dropdownLink}>
-                            Lost
-                        </Link>,
-                        <Link to="/volunteer-page" className={classes.dropdownLink}>
-                            Volunteer
-                        </Link>,
-                    ]}
-                />
-            </ListItem>
-        </List>
-    );
+      <ListItem className={classes.listItem}>
+        <CustomDropdown
+          noLiPadding
+          buttonText="More"
+          buttonProps={{
+            className: classes.navLink,
+            color: "transparent",
+          }}
+          buttonIcon={Apps}
+          dropdownList={[
+            <Link to="/profile-page" className={classes.dropdownLink}>
+              Profile
+            </Link>,
+            <Link to="/adopt-page" className={classes.dropdownLink}>
+              Adopt
+            </Link>,
+            <Link to="/lost-page" className={classes.dropdownLink}>
+              Lost
+            </Link>,
+            <Link to="/volunteer-page" className={classes.dropdownLink}>
+              Volunteer
+            </Link>,
+          ]}
+        />
+      </ListItem>
+    </List>
+  );
 }
