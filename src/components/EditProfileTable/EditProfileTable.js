04c721337a72394a357c9a61880221e21e30be38
serasalin96@gmail.com <serasalin96@gmail.com>
2020-06-08 11:58:57 +0300

    Carina

---
+++ src/components/EditProfileTable/EditProfileTable.js
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Form from "react-bootstrap/Form";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+});
+
+export default function SimpleTable() {
+    const classes = useStyles();
+
+    const [id, setId] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [photo, setPhoto] = useState("");
+    const [desc, setDesc] = useState("");
+    const [phone, setPhone] = useState("");
+    const [city, setCity] = useState("");
+
+    useEffect(() => {
+        const requestOptions = {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                token: localStorage.getItem("jwt-token"),
+            },
+        };
+
+        fetch("http://localhost:4000/user/me", requestOptions)
+            .then((response) => response.json())
+            .then((data) => handleResponse(data));
+    }, []);
+
+    function handleResponse(data) {
+        setId(data._id);
+        setName(data.username);
+        setEmail(data.email);
+        //setPhoto(data.photo);
+        setCity(data.city);
+        setDesc(data.desc);
+        setPhone(data.phone);
+    }
+
+    function handleSubmit(event) {
+        setName({ name: event.target.value });
+        setEmail({ email: event.target.value });
+        setPhoto({ photo: event.target.value });
+        setCity({ city: event.target.value });
+        setDesc({ desc: event.target.value });
+        setPhone({ phone: event.target.value });
+    }
+
+    return (
+        <Form>
+            <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control type="text" placeholder={name} value={name} />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder={email} value={email} />
+            </Form.Group>
+            <Form.Group>
+                <Form.File
+                    id="exampleFormControlFile1"
+                    label="Profile photo"
+                    value={photo}
+                />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Label>Phone number</Form.Label>
+                <Form.Control type="text" placeholder={phone} value={phone} />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Label>City</Form.Label>
+                <Form.Control type="text" placeholder={city} value={city} />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Description</Form.Label>
+                <Form.Control as="textarea" rows="3" placeholder={desc} value={desc} />
+            </Form.Group>
+            <Button onClick={handleSubmit}>Update profile</Button>
+        </Form>
+    );
+}
--- src/views/ProfilePage/ProfilePage.js
+++ src/views/ProfilePage/ProfilePage.js
@@ -2,12 +2,12 @@ import React from "react";
 // nodejs library that concatenates classes
 import classNames from "classnames";
 // @material-ui/core components
-import {makeStyles} from "@material-ui/core/styles";
-import {useState, useEffect} from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import { useState, useEffect } from "react";
 // @material-ui/icons
 import Home from "@material-ui/icons/Home";
 import AccountBox from "@material-ui/icons/AccountBox";
-import Pets from "@material-ui/icons/Pets"
+import Pets from "@material-ui/icons/Pets";
 // core components
 import Header from "components/Header/Header.js";
 import Footer from "components/Footer/Footer.js";
@@ -17,14 +17,20 @@ import HeaderLinks from "components/Header/HeaderLinks.js";
 import NavPills from "components/NavPills/NavPills.js";
 import Parallax from "components/Parallax/Parallax.js";
 
-import ForAdoptionTable from "../../components/ForAdoptionTable/ForAdoptionTable.js"
-import MyPetsTable from "../../components/MyPetsTable/MyPetsTable"
+import ForAdoptionTable from "../../components/ForAdoptionTable/ForAdoptionTable.js";
+import MyPetsTable from "../../components/MyPetsTable/MyPetsTable";
+import EditProfileTable from "../../components/EditProfileTable/EditProfileTable";
 
 import studio1 from "assets/img/examples/studio-1.jpg";
+import studio2 from "assets/img/examples/studio-2.jpg";
 import studio3 from "assets/img/examples/studio-3.jpg";
+import studio4 from "assets/img/examples/studio-4.jpg";
+import studio5 from "assets/img/examples/studio-5.jpg";
 import work1 from "assets/img/examples/olu-eletu.jpg";
 import work2 from "assets/img/examples/clem-onojeghuo.jpg";
+import work3 from "assets/img/examples/cynthia-del-rio.jpg";
 import work4 from "assets/img/examples/mariya-georgieva.jpg";
+import work5 from "assets/img/examples/clem-onojegaw.jpg";
 
 import styles from "assets/jss/material-kit-react/views/profilePage.js";
 
@@ -32,7 +38,7 @@ const useStyles = makeStyles(styles);
 
 export default function ProfilePage(props) {
     const classes = useStyles();
-    const {...rest} = props;
+    const { ...rest } = props;
     const imageClasses = classNames(
         classes.imgRaised,
         classes.imgRoundedCircle,
@@ -40,25 +46,25 @@ export default function ProfilePage(props) {
     );
     const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
 
-    const [name, setName] = useState('dummyName');
-    const [email, setEmail] = useState('dummyEmail');
-    const [photo, setPhoto] = useState('');
-    const [desc, setDesc] = useState('');
-    const [phone, setPhone] = useState('');
-    const [city, setCity] = useState('');
+    const [name, setName] = useState("dummyName");
+    const [email, setEmail] = useState("dummyEmail");
+    const [photo, setPhoto] = useState("");
+    const [desc, setDesc] = useState("");
+    const [phone, setPhone] = useState("");
+    const [city, setCity] = useState("");
 
     useEffect(() => {
         const requestOptions = {
-            method: 'GET',
+            method: "GET",
             headers: {
-                'Content-Type': 'application/json',
-                'token': localStorage.getItem("jwt-token")
-            }
+                "Content-Type": "application/json",
+                token: localStorage.getItem("jwt-token"),
+            },
         };
 
-        fetch('http://localhost:4000/user/me', requestOptions)
-            .then(response => response.json())
-            .then(data => handleResponse(data));
+        fetch("http://localhost:4000/user/me", requestOptions)
+            .then((response) => response.json())
+            .then((data) => handleResponse(data));
     }, []);
 
     function handleResponse(data) {
@@ -72,52 +78,54 @@ export default function ProfilePage(props) {
             <Header
                 color="transparent"
                 brand="Maynard Pet Shelter"
-                rightLinks={<HeaderLinks/>}
+                rightLinks={<HeaderLinks />}
                 fixed
                 changeColorOnScroll={{
                     height: 200,
-                    color: "white"
+                    color: "white",
                 }}
                 {...rest}
             />
-            <Parallax small filter image={require("assets/img/profile-bg.jpg")}/>
+            <Parallax small filter image={require("assets/img/profile-bg.jpg")} />
             <div className={classNames(classes.main, classes.mainRaised)}>
                 <div>
                     <div className={classes.container}>
                         <GridContainer justify="center">
                             <GridItem xs={12} sm={12} md={6}>
                                 <div className={classes.profile}>
-                                    {
-                                        photo &&
+                                    {photo && (
                                         <div>
-                                            <img src={`../img/${photo}`} alt="..." className={imageClasses}/>
+                                            <img
+                                                src={`../img/${photo}`}
+                                                alt="..."
+                                                className={imageClasses}
+                                            />
                                         </div>
-                                    }
-                                    {
-                                        photo &&
+                                    )}
+                                    {photo && (
                                         <div className={classes.name}>
                                             <h3 className={classes.title}>{name}</h3>
-                                            <h6>{desc}</h6>
+                                            <h6>PET LOVER</h6>
                                             <h6>{email}</h6>
                                             <h6>{city}</h6>
+                                            <h6>{desc}</h6>
                                             <h6>{phone}</h6>
                                         </div>
-                                    }
-                                    {
-                                        !photo &&
-                                        <div className={classes.name} style={{marginTop : "10px"}}>
+                                    )}
+                                    {!photo && (
+                                        <div className={classes.name} style={{ marginTop: "10px" }}>
                                             <h3 className={classes.title}>{name}</h3>
-                                            <h6>{desc}</h6>
+                                            <h6>PET LOVER</h6>
                                             <h6>{email}</h6>
                                             <h6>{city}</h6>
+                                            <h6>{desc}</h6>
                                             <h6>{phone}</h6>
                                         </div>
-                                    }
+                                    )}
                                 </div>
                             </GridItem>
                         </GridContainer>
-                        <div className={classes.description}>
-                        </div>
+                        <div className={classes.description}></div>
                         <GridContainer justify="center">
                             <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
                                 <NavPills
@@ -129,56 +137,28 @@ export default function ProfilePage(props) {
                                             tabIcon: Home,
                                             tabContent: (
                                                 <GridContainer justify="center">
-                                                    <ForAdoptionTable/>
+                                                    <ForAdoptionTable />
                                                 </GridContainer>
-                                            )
+                                            ),
                                         },
                                         {
                                             tabButton: "My Pets",
                                             tabIcon: Pets,
                                             tabContent: (
                                                 <GridContainer justify="center">
-                                                    <MyPetsTable/>
+                                                    <MyPetsTable />
                                                 </GridContainer>
-                                            )
+                                            ),
                                         },
                                         {
                                             tabButton: "Profile",
                                             tabIcon: AccountBox,
                                             tabContent: (
                                                 <GridContainer justify="center">
-                                                    <GridItem xs={12} sm={12} md={4}>
-                                                        <img
-                                                            alt="..."
-                                                            src={work4}
-                                                            className={navImageClasses}
-                                                        />
-                                                        <img
-                                                            alt="..."
-                                                            src={studio3}
-                                                            className={navImageClasses}
-                                                        />
-                                                    </GridItem>
-                                                    <GridItem xs={12} sm={12} md={4}>
-                                                        <img
-                                                            alt="..."
-                                                            src={work2}
-                                                            className={navImageClasses}
-                                                        />
-                                                        <img
-                                                            alt="..."
-                                                            src={work1}
-                                                            className={navImageClasses}
-                                                        />
-                                                        <img
-                                                            alt="..."
-                                                            src={studio1}
-                                                            className={navImageClasses}
-                                                        />
-                                                    </GridItem>
+                                                    <EditProfileTable />
                                                 </GridContainer>
-                                            )
-                                        }
+                                            ),
+                                        },
                                     ]}
                                 />
                             </GridItem>
@@ -186,7 +166,7 @@ export default function ProfilePage(props) {
                     </div>
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
 }
